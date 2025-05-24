45518cc9753e7a00e0b0160c18f1d8dfbb1fbcb4
Yaa_hosein <layali.14.peshawar@example.com>
2025-05-24 18:30:33 +0330

    adding not found page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { Route,createBrowserRouter,createRoutesFromElements,RouterProvider } fro
 import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
+import NotFoundPage from "./pages/NotFoundPage"
 
 const router=createBrowserRouter(
   createRoutesFromElements(
   <Route path="/" element={<MainLayout/>}>
   <Route index element={<HomePage/>}/>
   <Route path="/jobs" element={<JobsPage/>}/>
+  <Route path="*" element={<NotFoundPage/>}/>
   
   </Route>
 ))
