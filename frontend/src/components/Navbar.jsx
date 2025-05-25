559224acd1805e047a9fa55b7b43afe59905d4f9
Yaa_hosein <layali.14.peshawar@example.com>
2025-05-25 10:40:30 +0330

    less code and defining a linkClass func

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,12 @@
 import logo from "../assets/images/logo.png";
-import { NavLink, Link } from "react-router-dom";
+import { NavLink} from "react-router-dom";
 
 const Navbar = () => {
+  const linkClass=({ isActive }) =>
+                isActive
+                  ? "bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                  : "text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+              
   return (
     <nav className="bg-indigo-700 border-b border-indigo-500">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -19,11 +24,7 @@ const Navbar = () => {
             <div className="md:ml-auto">
               <div className="flex space-x-2">
                 <NavLink
-                className={({ isActive }) =>
-                isActive
-                  ? "bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  : "text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-              }
+                className={linkClass}
                   to="/"
                   
                 >
@@ -31,13 +32,13 @@ const Navbar = () => {
                 </NavLink>
                 <NavLink
                   to="/jobs"
-                  className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                  className={linkClass}
                 >
                   المواد
                 </NavLink>
                 <NavLink
                   to="/add-job"
-                  className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                  className={linkClass}
                 >
                   إضافة{" "}
                 </NavLink>
