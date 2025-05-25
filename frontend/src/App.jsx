71d0d724f769bc58b45e0a8348089ad91899f6ce
Yaa_hosein <layali.14.peshawar@example.com>
2025-05-25 17:16:27 +0330

    working on the single pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage"
+import JobPage from "./pages/JobPage";
 
 const router=createBrowserRouter(
   createRoutesFromElements(
   <Route path="/" element={<MainLayout/>}>
   <Route index element={<HomePage/>}/>
   <Route path="/jobs" element={<JobsPage/>}/>
+    <Route path="/jobs/:id" element={<JobPage/>}/>
   <Route path="*" element={<NotFoundPage/>}/>
   
   </Route>
---
+++ frontend/src/pages/JobPage.jsx
@@ -0,0 +1,8 @@
+
+const JobPage = () => {
+  return (
+    <div>JobPage</div>
+  )
+}
+
+export default JobPage
