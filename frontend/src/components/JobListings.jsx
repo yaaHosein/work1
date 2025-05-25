ab7820e5ed02a072c3d328ca81f1337473d47387
Yaa_hosein <layali.14.peshawar@example.com>
2025-05-25 15:38:22 +0330

    implementing المواد page

--- frontend/src/components/JobListings.jsx
+++ frontend/src/components/JobListings.jsx
@@ -1,7 +1,7 @@
 import jobs from "../jobs.json";
 import JobListing from "./JobListing";
-const JobListings = () => {
-  const recentJobs = jobs.slice(0, 3);
+const JobListings = ({isHome=false}) => {
+  const jobListings =isHome? jobs.slice(0, 3):jobs;
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
@@ -9,7 +9,7 @@ const JobListings = () => {
           Browse Jobs
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {recentJobs.map((job) => (
+          {jobListings.map((job) => (
             <JobListing key={job.id} job={job} />
           ))}
         </div>
