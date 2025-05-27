99cfbcf1e0db26986ec7e3b062a600e5dcc73332
Yaa_hosein <layali.14.peshawar@example.com>
2025-05-27 10:32:40 +0330

    commenting useState and useEffects codes and trying to show the first job in another way

--- frontend/src/pages/JobPage.jsx
+++ frontend/src/pages/JobPage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Spinner from "../components/Spinner";
 // import { FaArrowLeft } from "react-icons/fa";
@@ -6,23 +6,23 @@ import Spinner from "../components/Spinner";
 
 const JobPage = () => {
   const { id } = useParams();
-  const [job, setJob] = useState(null);
-  const [loading, setLoading] = useState(true);
+  // const [job, setJob] = useState(null);
+  // const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchJob = async () => {
-      try {
-        const res = await fetch(`/api/jobs/${id}`);
-        const data = await res.json();
-        setJob(data);
-      } catch (error) {
-        console.log("Error fetching data", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchJob();
-  }, []);  
+  // useEffect(() => {
+  //   const fetchJob = async () => {
+  //     try {
+  //       const res = await fetch(`/api/jobs/${id}`);
+  //       const data = await res.json();
+  //       setJob(data);
+  //     } catch (error) {
+  //       console.log("Error fetching data", error);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
+  //   fetchJob();
+  // }, []);  
   return loading?<Spinner/>: ( 
         <>
     <h1>{job.title}</h1>
