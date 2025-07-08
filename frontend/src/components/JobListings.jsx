7eaa7b687d2f2dbf4eca688cff5e14a2a5bc2438
Yaa_hosein <layali.14.peshawar@example.com>
2025-07-08 11:11:20 +0330

    replacing fetchApi function name and changing its location

--- frontend/src/components/JobListings.jsx
+++ frontend/src/components/JobListings.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 
 const JobListings = ({ isHome = false }) => {
   const [jobs, setJobs] = useState([]);
-const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
   // useEffect(() => {
   //   const fetchJobs = async () => {
@@ -25,40 +25,35 @@ const [loading, setLoading] = useState(true);
   //   fetchJobs();
   // }, []);
 
-  const fetchApI= async ()=>{
-    const response= await axios.get("http://localhost:8080/jobs") 
-    setJobs(response.data.jobs);
-    console.log(response.data.jobs);
-  }
+  useEffect(() => {
+    const fetchJobs = async () => {
+      const res = await axios.get("http://localhost:8080/jobs");
+      setJobs(res.data.jobs);
+      console.log(res.data.jobs);
+    };
 
-  useEffect(()=>{
-    fetchApI();
-  },[])
+    setLoading(false);
+    fetchJobs();
+  }, []);
   return (
-
-  
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
           {isHome ? "Recent Jobs" : "Browse Jobs"}
         </h2>
-        
-          {loading ? (
-            <Spinner loading={loading}/>
-          ) : (
+
+        {loading ? (
+          <Spinner loading={loading} />
+        ) : (
           <div className="grid grid-c ols-1 md:grid-cols-3 gap-6">
-              {jobs.map((job) => (
-                <JobListing key={job.id} job={job} />
-              ))}
-            </div>
-          )}
+            {jobs.map((job) => (
+              <JobListing key={job.id} job={job} />
+            ))}
+          </div>
+        )}
       </div>
-    </section> 
+    </section>
   );
 };
 
 export default JobListings;
-
-
-
-
