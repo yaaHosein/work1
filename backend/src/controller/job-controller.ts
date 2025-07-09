2a732150ee45f1ad84d31ca8e36fdeb4dc835d41
Yaa_hosein <layali.14.peshawar@example.com>
2025-07-09 11:10:23 +0330

    adding jos-controller file

---
+++ backend/src/controller/job-controller.ts
@@ -0,0 +1,25 @@
+// const jobs: Job[] = [
+//   {
+//     id: v7(),
+//     title: "Software Engineer",
+//     company: "tec crop",
+//   },
+//   {
+//     id: v7(),
+//     title: "teacher",
+//     company: "Bangor",
+//   },
+//   {
+//     id: v7(),
+//     title: "Dr",
+//     company: "matrix",
+//   },
+// ];
+// export class JobController{
+//     constructor(private JobService:Any){}
+
+//     getJobs(request:Request,response:Response){
+//         return jobs}
+    
+
+// }
