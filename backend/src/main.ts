b76329dd8cc1b179ab1a13f920f2404311c177f4
Yaa_hosein <layali.14.peshawar@example.com>
2025-07-12 12:03:24 +0330

    practicing

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,77 +12,55 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 const jobs: Job[] = [
-  {
-    id: v7(),
-    title: "Senior React Developer",
-    type: "Full-Time",
-    description:
-      "We are seeking a talented Front-End Developer to join our team in Boston, MA. The ideal candidate will have strong skills in HTML, CSS, and JavaScript, with experience working with modern JavaScript frameworks such as React or Angular.",
-    location: "Boston, MA",
-    salary: "$70K - $80K",
-    company: {
-      name: "NewTek Solutions",
-      description:
-        "NewTek Solutions is a leading technology company specializing in web development and digital solutions. We pride ourselves on delivering high-quality products and services to our clients while fostering a collaborative and innovative work environment.",
-      contactEmail: "contact@teksolutions.com",
-      contactPhone: "555-555-5555",
+    {
+        id: "0197fd96-7a50-74d6-94bb-c4a5b0b8d515",
+        title: "Senior React Developer",
+        type: "Full-Time",
+        description: "We are seeking a talented Front-End Developer to join our team in Boston, MA. The ideal candidate will have strong skills in HTML, CSS, and JavaScript, with experience working with modern JavaScript frameworks such as React or Angular.",
+        location: "Boston, MA",
+        salary: "$70K - $80K",
+        company: {
+            name: "NewTek Solutions",
+            description: "NewTek Solutions is a leading technology company specializing in web development and digital solutions. We pride ourselves on delivering high-quality products and services to our clients while fostering a collaborative and innovative work environment.",
+            contactEmail: "contact@teksolutions.com",
+            contactPhone: "555-555-5555"
+        }
     },
-  },
-  {
-    id: v7(),
-    title: "Front-End Engineer (React & Redux)",
-    type: "Full-Time",
-    location: "Miami, FL",
-    description:
-      "Join our team as a Front-End Developer in sunny Miami, FL. We are looking for a motivated individual with a passion for crafting beautiful and responsive web applications. Experience with UI/UX design principles and a strong attention to detail are highly desirable.",
-    salary: "$70K - $80K",
-    company: {
-      name: "Veneer Solutions",
-      description:
-        "Veneer Solutions is a creative agency specializing in digital design and development. Our team is dedicated to pushing the boundaries of creativity and innovation to deliver exceptional results for our clients.",
-      contactEmail: "contact@loremipsum.com",
-      contactPhone: "555-555-5555",
+    {
+        id: "0197fd96-7a52-77a5-ac8f-8b594a058268",
+        title: "Front-End Engineer (React & Redux)",
+        type: "Full-Time",
+        location: "Miami, FL",
+        description: "Join our team as a Front-End Developer in sunny Miami, FL. We are looking for a motivated individual with a passion for crafting beautiful and responsive web applications. Experience with UI/UX design principles and a strong attention to detail are highly desirable.",
+        salary: "$70K - $80K",
+        company: {
+            name: "Veneer Solutions",
+            description: "Veneer Solutions is a creative agency specializing in digital design and development. Our team is dedicated to pushing the boundaries of creativity and innovation to deliver exceptional results for our clients.",
+            contactEmail: "contact@loremipsum.com",
+            contactPhone: "555-555-5555"
+        }
     },
-  },
-  {
-    id: v7(),
-    title: "Full Stack Reac",
-    type: "Full-Time",
-    location: "Atlanta,",
-    description:
-      "Exciting opportunity for a Full-Time Front-End Developer in bustling Atlanta, GA. We are seeking a talented individual with a passion for building elegant and scalable web applications. Join our team and make an impact!",
-    salary: "$90K - $100K",
-    company: {
-      name: "Browning Technologies",
-      description:
-        "Browning Technologies is a rapidly growing technology company specializing in e-commerce solutions. We offer a dynamic and collaborative work environment where employees are encouraged to think creatively and innovate.",
-      contactEmail: "contact@consecteturadipisicing.com",
-      contactPhone: "555-555-5555",
-    },
-  },
-  {
-    id: v7(),
-    title: "React Native Developer",
-    type: "Full-Time",
-    description:
-      "Join our team as a Front-End Developer in beautiful Portland, OR. We are looking for a skilled and enthusiastic individual to help us create innovative web solutions. Competitive salary and great benefits package available.",
-    location: "Portland, OR",
-    salary: "$100K - $110K",
-    company: {
-      name: "Port Solutions INC",
-      description:
-        "Port Solutions is a leading technology company specializing in software development and digital marketing. We are committed to providing our clients with cutting-edge solutions and our employees with a supportive and rewarding work environment.",
-      contactEmail: "contact@ipsumlorem.com",
-      contactPhone: "555-555-5555",
-    },
-  },
-];
+    {
+        id: "0197fd96-7a52-77a5-ac8f-8d660a1c9b8e",
+        title: "Full Stack Reac",
+        type: "Full-Time",
+        location: "Atlanta,",
+        description: "Exciting opportunity for a Full-Time Front-End Developer in bustling Atlanta, GA. We are seeking a talented individual with a passion for building elegant and scalable web applications. Join our team and make an impact!",
+        salary: "$90K - $100K",
+        company: {
+            name: "Browning Technologies",
+            description: "Browning Technologies is a rapidly growing technology company specializing in e-commerce solutions. We offer a dynamic and collaborative work environment where employees are encouraged to think creatively and innovate.",
+            contactEmail: "contact@consecteturadipisicing.com",
+            contactPhone: "555-555-5555"
+        }
+    }
+]
 
-app.get("/jobs", (req, res) => {
+app.get("/api/jobs", (req, res) => {
   res.json(jobs);
 });
 
-app.get("/jobs/:id", (req, res) => {
+app.get("/api/jobs/:id", (req, res) => {
   const jobId = req.params.id
   const job = jobs.find((j) => j.id === jobId);
   if (job) {
@@ -92,12 +70,12 @@ app.get("/jobs/:id", (req, res) => {
   }
 });
 
-// app.post("jobs", express.json(), (req, res) => {
-//     const newJob:Job = req.body;
-//     newJob.id = v7(); // simple Id generation
-//     jobs.push(newJob);
-//     res.status(201).json(newJob);
-// });
+app.post("/api/jobs", express.json(), (req, res) => {
+    const newJob:Job = req.body;
+    newJob.id = v7(); // simple Id generation
+    jobs.push(newJob);
+    res.status(201).json(newJob);
+});
 
 app.listen(1100, () => {
   console.log("server is running on port 1100");
