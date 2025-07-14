140ed435530f511702e4b963d5402d2430a11042
Yaa_hosein <layali.14.peshawar@example.com>
2025-07-14 14:25:09 +0330

    updating and deleting jobs

--- backend/src/controller/job-controller.ts
+++ backend/src/controller/job-controller.ts
@@ -1,65 +1,117 @@
-import { Router,Request,Response, request } from "express";
-import { Job } from "../types/job.js"
+import { Router, Request, Response, request } from "express";
+import { Job } from "../types/job.js";
+import { v7 } from "uuid";
 
-// const jobs: Job[] = [
-//     {
-//         "id": "0197fd96-7a50-74d6-94bb-c4a5b0b8d515",
-//         "title": "Senior React Developer",
-//       "type": "Full-Time",
-//       "description": "We are seeking a talented Front-End Developer to join our team in Boston, MA. The ideal candidate will have strong skills in HTML, CSS, and JavaScript, with experience working with modern JavaScript frameworks such as React or Angular.",
-//       "location": "Boston, MA",
-//       "salary": "$70K - $80K",
-//       "company": {
-//         "name": "NewTek Solutions",
-//         "description": "NewTek Solutions is a leading technology company specializing in web development and digital solutions. We pride ourselves on delivering high-quality products and services to our clients while fostering a collaborative and innovative work environment.",
-//         "contactEmail": "contact@teksolutions.com",
-//         "contactPhone": "555-555-5555"
-//         }
-//     },
-//     {
-//         "id": "0197fd96-7a52-77a5-ac8f-8b594a058268",
-//        "title": "Front-End Engineer (React & Redux)",
-//       "type": "Full-Time",
-//       "location": "Miami, FL",
-//       "description": "Join our team as a Front-End Developer in sunny Miami, FL. We are looking for a motivated individual with a passion for crafting beautiful and responsive web applications. Experience with UI/UX design principles and a strong attention to detail are highly desirable.",
-//       "salary": "$70K - $80K",
-//       "company": {
-//         "name": "Veneer Solutions",
-//         "description": "Veneer Solutions is a creative agency specializing in digital design and development. Our team is dedicated to pushing the boundaries of creativity and innovation to deliver exceptional results for our clients.",
-//         "contactEmail": "contact@loremipsum.com",
-//         "contactPhone": "555-555-5555"
-//         }
-//     },
-//     {
-//         "id": "0197fd96-7a52-77a5-ac8f-8d660a1c9b8e",
-//           "title": "Full Stack Reac",
-//       "type": "Full-Time",
-//       "location": "Atlanta,",
-//       "description": "Exciting opportunity for a Full-Time Front-End Developer in bustling Atlanta, GA. We are seeking a talented individual with a passion for building elegant and scalable web applications. Join our team and make an impact!",
-//       "salary": "$90K - $100K",
-//       "company": {
-//         "name": "Browning Technologies",
-//         "description": "Browning Technologies is a rapidly growing technology company specializing in e-commerce solutions. We offer a dynamic and collaborative work environment where employees are encouraged to think creatively and innovate.",
-//         "contactEmail": "contact@consecteturadipisicing.com",
-//         "contactPhone": "555-555-5555"
-//         }
-//     }
-// ]
+const jobs: Job[] = [
+  {
+    id: "0197fd96-7a50-74d6-94bb-c4a5b0b8d515",
+    title: "Senior React Developer",
+    type: "Full-Time",
+    description:
+      "We are seeking a talented Front-End Developer to join our team in Boston, MA. The ideal candidate will have strong skills in HTML, CSS, and JavaScript, with experience working with modern JavaScript frameworks such as React or Angular.",
+    location: "Boston, MA",
+    salary: "$70K - $80K",
+    company: {
+      name: "NewTek Solutions",
+      description:
+        "NewTek Solutions is a leading technology company specializing in web development and digital solutions. We pride ourselves on delivering high-quality products and services to our clients while fostering a collaborative and innovative work environment.",
+      contactEmail: "contact@teksolutions.com",
+      contactPhone: "555-555-5555",
+    },
+  },
+  {
+    id: "0197fd96-7a52-77a5-ac8f-8b594a058268",
+    title: "Front-End Engineer (React & Redux)",
+    type: "Full-Time",
+    location: "Miami, FL",
+    description:
+      "Join our team as a Front-End Developer in sunny Miami, FL. We are looking for a motivated individual with a passion for crafting beautiful and responsive web applications. Experience with UI/UX design principles and a strong attention to detail are highly desirable.",
+    salary: "$70K - $80K",
+    company: {
+      name: "Veneer Solutions",
+      description:
+        "Veneer Solutions is a creative agency specializing in digital design and development. Our team is dedicated to pushing the boundaries of creativity and innovation to deliver exceptional results for our clients.",
+      contactEmail: "contact@loremipsum.com",
+      contactPhone: "555-555-5555",
+    },
+  },
+  {
+    id: "0197fd96-7a52-77a5-ac8f-8d660a1c9b8e",
+    title: "Full Stack Reac",
+    type: "Full-Time",
+    location: "Atlanta,",
+    description:
+      "Exciting opportunity for a Full-Time Front-End Developer in bustling Atlanta, GA. We are seeking a talented individual with a passion for building elegant and scalable web applications. Join our team and make an impact!",
+    salary: "$90K - $100K",
+    company: {
+      name: "Browning Technologies",
+      description:
+        "Browning Technologies is a rapidly growing technology company specializing in e-commerce solutions. We offer a dynamic and collaborative work environment where employees are encouraged to think creatively and innovate.",
+      contactEmail: "contact@consecteturadipisicing.com",
+      contactPhone: "555-555-5555",
+    },
+  },
+];
 
-export class JobController{
-    constructor(router:Router){
-        router.get("/",this.getJobs)
+export class JobController {
+  public static readonly path = "/jobs";
+
+  constructor(router: Router) {
+    router.get("/", this.getJobs);
+    router.get("/:id", this.getJobById);
+    router.post("/", this.createJob);
+    router.put("/:id", this.updateJob);
+    router.delete("/:id", this.deleteJob);
+  }
+
+  getJobs(_request: Request, response: Response) {
+    response.json(jobs);
+  }
+
+  getJobById(request: Request, response: Response) {
+    const jobId = request.params.id;
+    const job = jobs.find((j) => j.id === jobId);
+    job
+      ? response.json(job)
+      : response.status(404).json({ message: "job not found" });
+  }
+
+  createJob(request: Request, response: Response) {
+    const newJob: Job = request.body;
+    newJob.id = v7();
+
+    jobs.push(newJob);
+    response.status(201).json(newJob);
+  }
+
+  updateJob(request: Request, response: Response) {
+    const jobId = request.params.id;
+    const jobIndex = jobs.findIndex((j) => j.id === jobId);
+
+    if (jobIndex === -1) {
+      response.status(404).json({ message: "Job not found" });
+      return;
     }
+    const updateJob: Job = request.body;
+    updateJob.id = jobId;
+    jobs[jobIndex] = updateJob;
+    response.json(updateJob);
 
-    getJobs(_request:Request,response:Response){
-        response.json(jobs)}
+    console.log("updating job with id ", jobId);
+    console.log("updated job data ", updateJob);
+  }
 
-        getJobById(
-            _request:Request,response:Response
-        ){
-            const jobId=request.params.id
-            const job=jobs.find
-        }
     
+  deleteJob(request:Request,response:Response){
+    const jobId=request.params.id;
+    const jobIndex= jobs.findIndex((j)=>j.id===jobId)
+    if (jobIndex===-1){
+ response.status(404).json({message:"Job not found"});
+ return
+    }
+  jobs.splice(jobIndex,1)
+  response.status(204).send()
+  }
+
 
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,20 +1,21 @@
 import express from "express";
 import cors from "cors";
-import { v7 } from "uuid";
 import { JobController } from "./controller/job-controller.js";
-import { Job } from "./types/job.js";
+// import { v7 } from "uuid";
+
+// import { Job } from "./types/job.js";
 
 var app = express();
+app.use(express.json())
+const JobsRouter=express.Router();
+new JobController(JobsRouter)
 
-const corsOptions = {
-  origin: ["http://localhost:5008"],
-};
+app.use(`/api${JobController.path}`,JobsRouter)
 
-app.use(cors(corsOptions));
 
-app.get("/api/jobs", (req, res) => {
-  res.json(jobs);
-});
+// app.get("/api/jobs", (req, res) => {
+//   res.json(jobs);
+// });
 
 // app.get("/api/jobs/:id", (req, res) => {
 //   const jobId = req.params.id
@@ -26,16 +27,26 @@ app.get("/api/jobs", (req, res) => {
 //   }
 // });
 
-const jobs: Job[] = []
+// const jobs: Job[] = []
 
 
 
-app.post("/api/jobs", express.json(), (req, res) => {
-    const newJob:Job = req.body;
-    newJob.id = v7(); // simple Id generation
-    jobs.push(newJob);
-    res.status(201).json(newJob);
-});
+// app.post("/api/jobs", express.json(), (req, res) => {
+//     const newJob:Job = req.body;
+//     newJob.id = v7(); // simple Id generation
+//     jobs.push(newJob);
+//     res.status(201).json(newJob);
+// });
+
+
+
+
+const corsOptions = {
+  origin: ["http://localhost:5008"],
+};
+
+app.use(cors(corsOptions));
+
 
 app.listen(1100, () => {
   console.log("server is running on port 1100");
