70c1eb8472de972357c6f1115717fd92bccf6107
Yaa_hosein <layali.14.peshawar@example.com>
2025-07-20 17:52:28 +0330

    logging jobId

--- frontend/src/providers/jobs-provider.ts
+++ frontend/src/providers/jobs-provider.ts
@@ -12,6 +12,8 @@ export class JobsProvider{
         }
 
         async getJobDetails(jobId:string):Promise<ApiJob>{
+            console.log(jobId);
+            
             const response = await fetch (`${this.endpoint}${JOB_URL}/${jobId}`);
             if (!response.ok){
                 throw new Error(`Error fetching job details:${response.statusText}`);
