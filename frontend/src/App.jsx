e2142bdf272a5195e3d41ecf0240e2042404a7d7
Yaa_hosein <layali.14.peshawar@example.com>
2025-07-23 09:35:35 +0330

    commenting extra codes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,35 +19,35 @@ const App = () => {
   const addJob = async (newJob) => {
     console.log(newJob);
 
-    const res = await fetch("/api/jobs", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newJob),
-    });
-    return;
+    // const res = await fetch("/api/jobs", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(newJob),
+    // });
+    // return;
   };
 
   //delete job:
   const deleteJob = async (id) => {
-    console.log("delete", id);
-    const res = await fetch(`/api/jobs/${id}`, {
-      method: "DELETE",
-    });
-    return;
+    // console.log("delete", id);
+    // const res = await fetch(`/api/jobs/${id}`, {
+    //   method: "DELETE",
+    // });
+    // return;
   };
 
   // edit job
   const updateJob = async (job) => {
-    const res = await fetch(`/api/jobs/${job.id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(job),
-    });
-    return;
+    // const res = await fetch(`/api/jobs/${job.id}`, {
+    //   method: "PUT",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(job),
+    // });
+    // return;
   };
 
   const router = createBrowserRouter(
