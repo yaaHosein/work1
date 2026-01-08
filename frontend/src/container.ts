a992ae092fdfbfb3f408b64ac128b9a63fa44de2
Yaa_hosein <layali.14.peshawar@example.com>
2026-01-08 14:35:01 +0330

    fixing url and endpoint

--- frontend/src/container.ts
+++ frontend/src/container.ts
@@ -1,8 +1,8 @@
 import { JobsProvider } from "./providers/jobs-provider";
 import { JobsService } from "./services/jobs-service";
 
-const API_ENDPOINT = "http://localhost:1100/api";
+const API_URL = "http://localhost:1100/api";
 
 export const container = {
-  JobsService: new JobsService(new JobsProvider(API_ENDPOINT)),
+  JobsService: new JobsService(new JobsProvider(API_URL)),
 };
--- frontend/src/providers/jobs-provider.ts
+++ frontend/src/providers/jobs-provider.ts
@@ -1,18 +1,18 @@
 import { ApiJob } from "../types/api-job";
 
-const JOB_URL = "/jobs";
+const JOB_ENDPOINT = "/jobs";
 
 export class JobsProvider {
   constructor(private endpoint: string) {}
 
   async getJobs(): Promise<ApiJob[]> {
-    const response = await fetch(`${this.endpoint}${JOB_URL}`);
+    const response = await fetch(`${this.endpoint}${JOB_ENDPOINT}`);
     const data = await response.json();
     return data;
   }
 
   async getJobDetails(jobId: string): Promise<ApiJob> {
-    const response = await fetch(`${this.endpoint}${JOB_URL}/${jobId}`);
+    const response = await fetch(`${this.endpoint}${JOB_ENDPOINT}/${jobId}`);
 
     if (!response.ok) {
       throw new Error(`Error fetching job details:${response.statusText}`);
